d8500ba71031db2ba9cd0920f3ed95a28d5900d2
unknown <Riyanagwl@gmail.com>
2022-12-16 19:50:50 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+import React from 'react';
+import './styles/style.css';
+ 
+function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <img src="./logo192.png" width="40px" alt=""/>
+                <ul className="nav-items">
+                    <li className="item">Pricing</li>
+                    <li className="item">About</li>
+                    <li className="item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+export default Page;
+
