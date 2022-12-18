0c6a165f4d7ad2530f190921106f5b7473de9e34
unknown <Riyanagwl@gmail.com>
2022-12-18 17:19:51 +0300

    add new files

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
 
-import React from 'react';
-import './styles/style.css';
- 
-function Header() {
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+export default function App() {
     return (
-        <header>
-            <nav className="navbar">
-                <img src="./logo192.png" width="40px" alt=""/>
-                <ul className="nav-items">
-                    <li className="item">Pricing</li>
-                    <li className="item">About</li>
-                    <li className="item">Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2021 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
+        <div className="container">
+            <Navbar />
+            <Main />
         </div>
     )
 }
-
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-export default Page;
-
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+  return <div>
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+    <ul className="main--facts">
+      <li>was first released in 2013</li>
+      <li>was originally created by jordan walk</li>
+      <li>Has well has 100k stars on github</li>
+      <li>Is maintained by facebook</li>
+      <li>powers over 100 mobile apps</li>
+    </ul>
+    </main>
+  </div>
+}
